82cd94b0ac346b2243886933112d5a0a933a9c0e
edufigura <edu.figueroas@duocuc.cl>
2026-05-15 00:05:16 -0400

    s

--- src/main/java/DuocQuin/Notificaciones/controller/notificacionesController.java
+++ src/main/java/DuocQuin/Notificaciones/controller/notificacionesController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import DuocQuin.Notificaciones.model.NotificacionModel;
 import DuocQuin.Notificaciones.service.NotificacionesService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
