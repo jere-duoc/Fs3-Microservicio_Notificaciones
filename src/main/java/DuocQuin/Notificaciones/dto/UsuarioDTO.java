d4c8097f9e71b6a3afbce9bedb2b32bfbc9eaa5d
Gislaine-P <gis.paredes@duocuc.cl>
2026-06-22 22:36:35 -0400

    Notificaciones aws ecr

--- src/main/java/DuocQuin/Notificaciones/dto/UsuarioDTO.java
+++ src/main/java/DuocQuin/Notificaciones/dto/UsuarioDTO.java
@@ -1,10 +1,5 @@
 package DuocQuin.Notificaciones.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class UsuarioDTO {
     private Long idUsuario;
     private String primerNombre;
@@ -12,4 +7,28 @@ public class UsuarioDTO {
     private String correoElectronico;
     private String telefonoCelular;
 
+    public UsuarioDTO() {}
+
+    public UsuarioDTO(Long idUsuario, String primerNombre, String segundoNombre, String correoElectronico, String telefonoCelular) {
+        this.idUsuario = idUsuario;
+        this.primerNombre = primerNombre;
+        this.segundoNombre = segundoNombre;
+        this.correoElectronico = correoElectronico;
+        this.telefonoCelular = telefonoCelular;
+    }
+
+    public Long getIdUsuario() { return idUsuario; }
+    public void setIdUsuario(Long idUsuario) { this.idUsuario = idUsuario; }
+
+    public String getPrimerNombre() { return primerNombre; }
+    public void setPrimerNombre(String primerNombre) { this.primerNombre = primerNombre; }
+
+    public String getSegundoNombre() { return segundoNombre; }
+    public void setSegundoNombre(String segundoNombre) { this.segundoNombre = segundoNombre; }
+
+    public String getCorreoElectronico() { return correoElectronico; }
+    public void setCorreoElectronico(String correoElectronico) { this.correoElectronico = correoElectronico; }
+
+    public String getTelefonoCelular() { return telefonoCelular; }
+    public void setTelefonoCelular(String telefonoCelular) { this.telefonoCelular = telefonoCelular; }
 }
